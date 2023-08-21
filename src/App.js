78c769772087b78c9d4b682bcf16e450e5aa49b1
kettavan <108821853+manikbashaaa@users.noreply.github.com>
2023-08-21 12:26:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn DevOps
+          Learn AWSDEVOPS
         </a>
       </header>
     </div>
